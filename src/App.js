315a2be99e03a393d7ce4f2c65412e956d047af6
GeoffreyFRANZ <61542992+GeoffreyFRANZ@users.noreply.github.com>
2024-10-14 14:33:51 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+// App.js
+
 import logo from './logo.svg';
 import './App.css';
+import Input from "./Components/Input";
+import Button from "./Components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    <b>
+                        Edit <code style={{color: 'red'}}>src/App.js</code> and save to reload.
+                    </b>
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <div>
+                    <Input/>
+                    <Button/>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const  Button =  () =>
+{
+    return (
+        <button type={'submit'}>Add</button>
+    )
+}
+
+export default Button
